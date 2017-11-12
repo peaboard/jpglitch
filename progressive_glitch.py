51f11d31d014ba530f1af3a15f98a9a034703303
peaboard <peaboard.mail@gmail.com>
2017-11-13 02:29:23 +0530

    Add Image View

--- progressive_glitch.py
+++ progressive_glitch.py
@@ -7,13 +7,19 @@ except ImportError:
     from tkinter import *   ## notice lowercase 't' in tkinter here
     from tkinter.filedialog import askopenfilename
 
+from PIL import ImageTk, Image
+
 from jpglitch import Jpeg
+
+import webbrowser
+
+
 
 # The GUI component of the program
 class imageGUI:
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
+        self.master.title("A simple GUI")
 
         self.label = Label(master, text="Let's Glitch Images")
         self.label.pack()
@@ -26,6 +32,10 @@ class imageGUI:
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
+
+        # self.newWindow = Toplevel(self.master)
+        # self.app = imageViewer(self.newWindow)
+
 
     # This function is called when the glitch button is pressed
     def glitch(self):
@@ -54,13 +64,65 @@ class imageGUI:
         # Overwrite the original file with the glitched image so that we can continue glitching
         jpeg.save_image(self.filename)
 
+        #self.app.loadImage(self.filename)
+
+        self.image_view()
+
         output = "\nSucces!"
         print(output)
 
+
+        #webbrowser.open(self.filename)
+
+        #self.current_glitched_image.show()
+
+
+
     def image_load(self):
         self.filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+        #self.current_glitched_image = Image.open(self.filename)
+        #self.current_glitched_image.show()
         print(self.filename)
 
-root = Tk()
-my_gui = imageGUI(root)
-root.mainloop()
+        self.CharPhoto = ImageTk.PhotoImage(Image.open(self.filename))
+        self.ChLabel = Label(self.master, image = self.CharPhoto)
+        self.ChLabel.image = self.CharPhoto
+        self.ChLabel.configure(image=self.CharPhoto)
+        self.ChLabel.pack(side = "bottom", fill = "both", expand = "yes")
+
+    def image_view(self):
+        self.newImage = ImageTk.PhotoImage(Image.open(self.filename))
+        self.ChLabel.image = self.newImage
+        self.ChLabel.configure(image=self.newImage)
+
+
+
+
+
+# class imageViewer:
+#
+#     def __init__(self, master):
+#         # Window for viewing image
+#         self.master = master
+#
+#         self.master.title("Join")
+#         self.master.geometry("300x300")
+#         self.master.configure(background='grey')
+#
+#     def loadImage(self, filename):
+#
+#         print(filename)
+#         img = ImageTk.PhotoImage(Image.open(filename))
+#         self.panel = Label(self.master, image = img)
+#         self.panel.pack(side = "bottom", fill = "both", expand = "yes")
+#         print("Image Loaded")
+#         self.master.configure(background='black')
+
+def main():
+    root = Tk()
+    my_gui = imageGUI(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
